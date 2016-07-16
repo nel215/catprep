10cb1fad4d7a552d60f73b0126047efb87bc81c2
nel215 <otomo.yuhei@gmail.com>
2016-07-16 18:22:16 +0900

    added setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+
+setup(
+    name='catprep',
+    description='A preprocessing library for categorical variables',
+    version='0.0.1',
+    author='nel215',
+    author_email='otomo.yuhei@gmail.com',
+    url='https://github.com/nel215/catprep',
+    py_modules=['catprep'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+    ],
+    keywords=['machine learning', 'preprocessing'],
+)
